ac78483f5d38ba666938ae64f9c559360cde9d43
ketwalters <ketwalters@gmail.com>
2016-10-21 16:42:12 -0400

    add board

---
+++ unbeatable_tic_tac_toe.rb
@@ -0,0 +1,13 @@
+class Board
+  def initialize
+    @board = ["","","","","","","","",""]
+  end
+
+  def display
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end
+end
+
+a = Board.new
